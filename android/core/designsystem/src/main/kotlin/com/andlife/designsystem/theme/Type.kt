03cdd645c247e063b652125390fff009216efd95
seojw0124 <wjddn2000124@gmail.com>
2025-12-27 06:24:39 +0900

    chore: InvitationTypography에 Immutable 추가

--- android/core/designsystem/src/main/kotlin/com/andlife/designsystem/theme/Type.kt
+++ android/core/designsystem/src/main/kotlin/com/andlife/designsystem/theme/Type.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,6 +34,7 @@ object InvitationLineHeightRatio {
 fun TextStyle.withLineHeightRatio(ratio: Float): TextStyle =
     copy(lineHeight = fontSize * ratio)
 
+@Immutable
 data class InvitationTypography(
     val heading1: TextStyle,
     val heading2: TextStyle,
