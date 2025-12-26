8f3d57967d6539298ae77f5f8556179140e65e4f
seojw0124 <wjddn2000124@gmail.com>
2025-12-27 06:03:18 +0900

    feat: Dimension 정의

---
+++ android/core/designsystem/src/main/kotlin/com/andlife/designsystem/theme/Dimen.kt
@@ -0,0 +1,36 @@
+package com.andlife.designsystem.theme
+
+import androidx.compose.ui.unit.dp
+
+object InvitationSpacing {
+    val twoXSmall = 2.dp
+    val xSmall = 4.dp
+    val small = 8.dp
+    val medium = 12.dp
+    val large = 16.dp
+    val xLarge = 20.dp
+    val twoXLarge = 24.dp
+    val threeXLarge = 32.dp
+}
+
+object InvitationRadius {
+    val twoXSmall = 4.dp
+    val threeXSmall = 8.dp
+    val xSmall = 12.dp
+    val small = 16.dp
+    val medium = 20.dp
+    val large = 24.dp
+}
+
+object InvitationIconSize {
+    val xSmall = 16.dp
+    val small = 20.dp
+    val medium = 24.dp
+    val large = 32.dp
+    val xLarge = 48.dp
+}
+
+object InvitationStroke {
+    val small = 1.dp
+    val medium = 2.dp
+}
