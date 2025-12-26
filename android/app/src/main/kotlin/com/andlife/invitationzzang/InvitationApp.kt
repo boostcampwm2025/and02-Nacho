9bc10ff7d6c38b179900ecd7b0bde7d7bbe77098
kim dong kyung <kdkd422@naver.com>
2025-12-26 11:31:21 +0900

    feat: InvitationApp 정의
    Co-authored-by: seojw0124 <wjddn2000124@gmail.com>
    Co-authored-by: jm3789 <jm37890@gmail.com>
    Co-authored-by: ant5555 <ymo6886@gmail.com>
    Co-authored-by: CDHyun <pdchoirian@gmail.com>

---
+++ android/app/src/main/kotlin/com/andlife/invitationzzang/InvitationApp.kt
@@ -0,0 +1,17 @@
+package com.andlife.invitationzzang
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.andlife.invitationzzang.navigation.InvitationNavHost
+import com.andlife.invitationzzang.navigation.rememberInvitationNavigator
+
+@Composable
+fun InvitationApp(
+    modifier: Modifier = Modifier
+) {
+    val navigator = rememberInvitationNavigator()
+    InvitationNavHost(
+        navigator = navigator,
+        modifier = modifier
+    )
+}
