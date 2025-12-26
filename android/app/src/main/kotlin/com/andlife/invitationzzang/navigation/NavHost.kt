8adba08dd93f936c88d2670ea1bd9a26c13a4c74
kim dong kyung <kdkd422@naver.com>
2025-12-26 11:32:08 +0900

    feat: NavHost
    Co-authored-by: seojw0124 <wjddn2000124@gmail.com>
    Co-authored-by: jm3789 <jm37890@gmail.com>
    Co-authored-by: ant5555 <ymo6886@gmail.com>
    Co-authored-by: CDHyun <pdchoirian@gmail.com>

---
+++ android/app/src/main/kotlin/com/andlife/invitationzzang/navigation/NavHost.kt
@@ -0,0 +1,77 @@
+package com.andlife.invitationzzang.navigation
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.navigation.compose.NavHost
+import com.andlife.home.homeNavGraph
+import com.andlife.invitation.invitationNavGraph
+import com.andlife.myinvitation.myInvitationNavGraph
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+
+@Composable
+fun InvitationNavHost(
+    navigator: InvitationNavigator,
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        bottomBar = {
+            AnimatedVisibility(navigator.shouldShowBottomBar()) {
+                InvitationBottomBar(
+                    currentTab = navigator.currentTab,
+                    tabs = navigator.mainBottomTabs.toImmutableList(),
+                    onTabSelected = navigator::navigate,
+                    modifier = Modifier
+                )
+            }
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        NavHost(
+            modifier = Modifier,
+            navController = navigator.navController,
+            startDestination = navigator.startDestination
+        ) {
+            homeNavGraph(innerPadding)
+            invitationNavGraph(innerPadding)
+            myInvitationNavGraph(innerPadding)
+        }
+    }
+}
+
+@Composable
+private fun InvitationBottomBar(
+    currentTab: MainBottomTab?,
+    tabs: ImmutableList<MainBottomTab>,
+    onTabSelected: (MainBottomTab) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    NavigationBar(
+        modifier = modifier,
+    ) {
+        tabs.forEach { tab ->
+            NavigationBarItem(
+                selected = currentTab == tab,
+                onClick = { onTabSelected(tab) },
+                icon = {
+                    Icon(
+                        ImageVector.vectorResource(tab.iconResId),
+                        stringResource(tab.labelResId)
+                    )
+                },
+                label = {
+                    Text(text = stringResource(tab.labelResId))
+                },
+            )
+        }
+    }
+}
