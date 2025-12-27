a2353b1c5c4d19c7c392b5f23533394e79e5002a
ant5555 <ymo6886@gmail.com>
2025-12-27 13:59:21 +0900

    feat: InvitationTextField 컴포넌트 작성

---
+++ android/core/designsystem/src/main/kotlin/com/andlife/designsystem/component/InvitationTextField.kt
@@ -0,0 +1,87 @@
+package com.andlife.designsystem.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.andlife.designsystem.R
+import com.andlife.designsystem.theme.InvitationSpacing
+import com.andlife.designsystem.theme.InvitationTheme
+
+@Composable
+fun InvitationTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    singleLine: Boolean = true,
+    modifier: Modifier = Modifier
+){
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = {
+            Text(
+                text = placeholder,
+                style = InvitationTheme.typography.bodyMedium1
+            )
+        },
+        trailingIcon = trailingIcon,
+        isError = isError,
+        enabled = enabled,
+        readOnly = readOnly,
+        singleLine = singleLine,
+        textStyle = InvitationTheme.typography.bodyMedium1,
+        shape = InvitationTheme.shapes.extraSmall,
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedTextColor = InvitationTheme.colorScheme.textPrimary,
+            unfocusedTextColor = InvitationTheme.colorScheme.textPrimary,
+            focusedPlaceholderColor = InvitationTheme.colorScheme.textTertiary,
+            unfocusedPlaceholderColor = InvitationTheme.colorScheme.textTertiary,
+            unfocusedBorderColor = InvitationTheme.colorScheme.backgroundBorder,
+            focusedBorderColor = InvitationTheme.colorScheme.brandPrimary,
+        ),
+        modifier = modifier.fillMaxWidth()
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InvitationTextFieldPreview() {
+    InvitationTheme {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(InvitationSpacing.small)
+        ) {
+            var address = ""
+
+            InvitationTextField(
+                value = address,
+                onValueChange = { address = it },
+                placeholder = "주소를 검색해주세요",
+                trailingIcon = {
+                    InvitationIconButton(
+                        iconRes = R.drawable.ic_search_24,
+                        contentDescription = "검색",
+                        onClick = { }
+                    )
+                }
+            )
+
+            InvitationTextField(
+                value = "",
+                onValueChange = { address = it },
+                placeholder = "상세 주소",
+            )
+        }
+    }
+}
