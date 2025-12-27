f5e7a2f950cae11c37bdd72685455ffd7ac1da49
ant5555 <ymo6886@gmail.com>
2025-12-27 17:05:07 +0900

    feat: 초대장 리스트 InvitationListitem 컴포넌트 작성

---
+++ android/core/ui/src/main/kotlin/com/andlife/ui/component/invitation/InvitationListItem.kt
@@ -0,0 +1,168 @@
+package com.andlife.ui.component.invitation
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import coil3.compose.AsyncImage
+import com.andlife.designsystem.component.InvitationDdayChip
+import com.andlife.designsystem.theme.InvitationIconSize
+import com.andlife.designsystem.theme.InvitationSpacing
+import com.andlife.designsystem.theme.InvitationStroke
+import com.andlife.designsystem.theme.InvitationTheme
+import com.andlife.ui.R
+
+
+@Composable
+fun InvitationListitem(
+    imageUrl: String,
+    title: String,
+    dateTime: String,
+    hostName: String,
+    address: String,
+    ddayText: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        onClick = onClick,
+        shape = InvitationTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = InvitationTheme.colorScheme.backgroundPrimary
+        ),
+        border = BorderStroke(
+            InvitationStroke.small,
+            InvitationTheme.colorScheme.backgroundBorder
+        ),
+        modifier = modifier,
+    ) {
+        Column {
+            Box {
+                AsyncImage(
+                    model = imageUrl,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(16f / 9f)
+                )
+
+                IconButton(
+                    onClick = { },
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(InvitationSpacing.large)
+                        .background(
+                            color = InvitationTheme.colorScheme.backgroundPrimary,
+                            shape = InvitationTheme.shapes.extraLarge
+                        )
+                        .size(InvitationIconSize.large)
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_more_vert_24),
+                        contentDescription = null,
+                    )
+                }
+            }
+
+            Column(
+                modifier = Modifier.padding(InvitationSpacing.medium),
+                verticalArrangement = Arrangement.spacedBy(InvitationSpacing.small)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = title,
+                        style = InvitationTheme.typography.bodyLarge1,
+                        modifier = Modifier.weight(1f),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+
+                    InvitationDdayChip(
+                        label = ddayText
+                    )
+                }
+
+                Text(
+                    text = hostName,
+                    style = InvitationTheme.typography.bodyMedium2,
+                    color = InvitationTheme.colorScheme.textTertiary
+                )
+
+                IconTextRow(
+                    iconRes = R.drawable.ic_calendar_24,
+                    text = dateTime
+                )
+
+                IconTextRow(
+                    iconRes = R.drawable.ic_location_24,
+                    text = address
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun IconTextRow(
+    iconRes: Int,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(InvitationSpacing.xSmall)
+    ) {
+        Icon(
+            painter = painterResource(id = iconRes),
+            contentDescription = null,
+            tint = Color.Unspecified ///InvitationTheme.colorScheme.textSecondary
+        )
+
+        Text(
+            text = text,
+            style = InvitationTheme.typography.bodyMedium2,
+            color = InvitationTheme.colorScheme.textSecondary,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InvitationListitemPreview(){
+    InvitationTheme {
+        InvitationListitem(
+            imageUrl = "https://example.com/image.jpg",
+            title = "네부캠 송년회",
+            dateTime = "2025년 1월 15일 오후 1시",
+            hostName = "안드라이프",
+            address = "강남대로62길 23 4층 코드스쿼드",
+            ddayText = "D-3",
+            onClick = {},
+        )
+    }
+}
