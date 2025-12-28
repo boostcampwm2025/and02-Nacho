36312b52841250b82fbd942bbb3168cece5439d2
kim dong kyung <kdkd422@naver.com>
2025-12-28 13:58:23 +0900

    build: build-logic build.gradle.kts

---
+++ android/build-logic/convention/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.andlife.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation(libs.android.gradleApiPlugin)
+    implementation(libs.kotlin.gradlePlugin)
+    compileOnly(libs.compose.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
