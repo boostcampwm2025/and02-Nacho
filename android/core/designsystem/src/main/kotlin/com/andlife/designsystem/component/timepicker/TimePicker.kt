e50eaa97f3ebaad0ecba9c59ade8aa389e9986e5
seojw0124 <wjddn2000124@gmail.com>
2025-12-28 22:48:01 +0900

    fix: 12시 기준으로 오전/오후 분기 안 되는 버그 수정

--- android/core/designsystem/src/main/kotlin/com/andlife/designsystem/component/timepicker/TimePicker.kt
+++ android/core/designsystem/src/main/kotlin/com/andlife/designsystem/component/timepicker/TimePicker.kt
@@ -1,22 +1,30 @@
 package com.andlife.designsystem.component.timepicker
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.andlife.designsystem.preview.ThemePreview
+import com.andlife.designsystem.theme.InvitationSpacing
 import com.andlife.designsystem.theme.InvitationTheme
 
 @Composable
@@ -25,55 +33,147 @@ fun InvitationTimePicker(
     modifier: Modifier = Modifier,
 ) {
     Row(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        BasicScrollableColumn(
-            items = listOf("오전", "오후"),
+        AmPmColumn(
+            isPm = state.isPm,
+            onAmPmChange = { state.isPm = it },
             modifier = Modifier.weight(1f)
         )
-        BasicScrollableColumn(
-            items = (0..12).map { it.toString() },
+        HourColumn(
+            hour = state.hour,
+            onHourChange = { newHour12 ->
+                val newHour12 = newHour12
+                val oldHour12 = if (state.hour % 12 == 0) 12 else state.hour % 12
+
+                if ((oldHour12 == 11 && newHour12 == 12) || (oldHour12 == 12 && newHour12 == 11)) {
+                    state.isPm = !state.isPm
+                }
+
+                state.hour = when {
+                    state.isPm && newHour12 != 12 -> newHour12 + 12
+                    !state.isPm && newHour12 == 12 -> 0
+                    else -> newHour12
+                }
+            },
             modifier = Modifier.weight(1f)
         )
-        BasicScrollableColumn(
-            items = (0..59 step state.minuteInterval).map { it.toString().padStart(2, '0') },
+        MinuteColumn(
+            minute = state.minute,
+            onMinuteChange = { state.minute = it },
             modifier = Modifier.weight(1f)
         )
     }
 }
 
 @Composable
-private fun BasicScrollableColumn(
-    items: List<String>,
+fun AmPmColumn(
+    isPm: Boolean,
+    onAmPmChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val listState = rememberLazyListState()
+    BasicScrollableColumn(
+        items = listOf("오전", "오후"),
+        initialIndex = if (isPm) 1 else 0,
+        onItemSelected = { onAmPmChange(it == 1) },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun HourColumn(
+    hour: Int, // 24시간제 입력
+    onHourChange: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val hour12 = if (hour % 12 == 0) 12 else hour % 12
+    BasicScrollableColumn(
+        items = (1..12).map { it.toString() },
+        initialIndex = hour12 - 1,
+        onItemSelected = { index -> onHourChange(index + 1) }, // 여기서 12시간제 -> 24시간제 로직은 부모가 처리
+        modifier = modifier
+    )
+}
+
+@Composable
+fun MinuteColumn(
+    minute: Int,
+    onMinuteChange: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    BasicScrollableColumn(
+        items = (0..59).map { it.toString().padStart(2, '0') },
+        initialIndex = minute,
+        onItemSelected = { onMinuteChange(it) },
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun BasicScrollableColumn(
+    items: List<String>,
+    initialIndex: Int,
+    onItemSelected: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val itemHeight = 60.dp // TODO: 나중에 외부에서 받도록 변경
+    val listState = rememberLazyListState(initialFirstVisibleItemIndex = initialIndex)
     val snapBehavior = rememberSnapFlingBehavior(lazyListState = listState)
 
-    LazyColumn(
-        modifier = modifier.height(200.dp),
-        state = listState,
-        flingBehavior = snapBehavior,
-        contentPadding = PaddingValues(vertical = 80.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+    val currentIndex = listState.firstVisibleItemIndex
+
+    LaunchedEffect(currentIndex) {
+        onItemSelected(currentIndex)
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(itemHeight * 3),
+        contentAlignment = Alignment.Center
     ) {
-        items(
-            items = items,
-            key = { it }
-        ) { item ->
-            Box(
-                modifier = Modifier.fillMaxWidth(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = item)
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(itemHeight)
+                .padding(horizontal = InvitationSpacing.large),
+            color = InvitationTheme.colorScheme.backgroundBorder,
+            shape = InvitationTheme.shapes.small
+        ) {
+
+        }
+        LazyColumn(
+            state = listState,
+            flingBehavior = snapBehavior,
+            contentPadding = PaddingValues(vertical = itemHeight),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            itemsIndexed(items) { index, item ->
+                val isSelected = index == currentIndex
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(itemHeight)
+                        .clickable {
+                            // TODO: 클릭 시 해당 아이템으로 스크롤 이동
+                        },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = item,
+                        color = if (isSelected) InvitationTheme.colorScheme.textPrimary else InvitationTheme.colorScheme.textTertiary,
+                    )
+                }
             }
         }
     }
 }
 
-private fun LazyListState.getCentralItemIndex(): Int? {
+private fun LazyListState.getCenterItemIndex(): Int? {
     val viewportCenter = (layoutInfo.viewportStartOffset + layoutInfo.viewportEndOffset) / 2
     val centralItemInfo = layoutInfo.visibleItemsInfo.find { itemInfo ->
         val itemCenter = (itemInfo.offset + itemInfo.size / 2)
@@ -92,8 +192,25 @@ private fun InvitationTimePickerPreview() {
             minuteInterval = 5,
         )
 
-        InvitationTimePicker(
-            state = state,
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            InvitationTimePicker(
+                state = state,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+            Text(
+                text = "선택된 시간: ${state.hour12}시 ${state.minute}분 ${if (state.isPm) "오후" else "오전"}",
+                style = InvitationTheme.typography.bodyLargeMedium,
+                modifier = Modifier
+                    .padding(top = InvitationSpacing.large)
+            )
+        }
     }
 }
