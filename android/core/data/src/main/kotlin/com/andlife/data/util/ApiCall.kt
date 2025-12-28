2a71be89ac86e54284a1fca48a0d32cf32365c3b
kim dong kyung <kdkd422@naver.com>
2025-12-28 23:13:18 +0900

    fix: ApiCall Data 모듈로 이동(network에서 domain을 모름)

--- android/core/data/src/main/kotlin/com/andlife/data/util/ApiCall.kt
+++ android/core/data/src/main/kotlin/com/andlife/data/util/ApiCall.kt
@@ -1,4 +1,4 @@
-package com.andlife.network.util
+package com.andlife.data.util
 
 import com.andlife.domain.error.DataError
 import com.andlife.domain.util.Result
@@ -6,17 +6,24 @@ import com.andlife.network.model.BaseResponse
 import kotlinx.serialization.SerializationException
 import retrofit2.HttpException
 
-suspend fun <T> apiCall(call: suspend () -> BaseResponse<T>): Result<T, DataError> {
-
-    return try {
+suspend fun <T> apiCall(call: suspend () -> BaseResponse<T>): Result<T, DataError> =
+    try {
         val response = call()
+
         when (response.code) {
             in 200..299 -> {
-                if (response.data == null) Result.Error(DataError.Network.NOT_FOUND, response.message)
-                else Result.Success(response.data)
+                response.data?.let {
+                    Result.Success(it) // it은 여기서 T (non-nullable)
+                } ?: Result.Error(DataError.Network.NOT_FOUND, response.message)
             }
-            401 -> Result.Error(DataError.Network.UNAUTHORIZED, response.message)
-            else -> Result.Error(DataError.Network.UNKNOWN, response.message)
+
+            401 -> {
+                Result.Error(DataError.Network.UNAUTHORIZED, response.message)
+            }
+
+            else -> {
+                Result.Error(DataError.Network.UNKNOWN, response.message)
+            }
         }
     } catch (e: HttpException) {
         when (e.code()) {
@@ -29,4 +36,3 @@ suspend fun <T> apiCall(call: suspend () -> BaseResponse<T>): Result<T, DataErro
     } catch (e: Exception) {
         Result.Error(DataError.Network.UNKNOWN, e.message)
     }
-}
