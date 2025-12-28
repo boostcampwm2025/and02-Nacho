0df5f186b907fa095194dcb06e39b8d4a0d3bda2
kim dong kyung <kdkd422@naver.com>
2025-12-28 14:10:41 +0900

    build: app모듈 gradle.kts kotlinOptions -> compilerOptions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -28,23 +30,27 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
-    implementation(project(":feature:home"))
-    implementation(project(":feature:invitation"))
-    implementation(project(":feature:myinvitation"))
-    implementation(project(":core:designsystem"))
+    implementation(projects.feature.home)
+    implementation(projects.feature.invitation)
+    implementation(projects.feature.myinvitation)
+    implementation(projects.core.designsystem)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlinx.immutable)
