3242117d8d101f8b4716d49fb490e314a38adadd
ant5555 <ymo6886@gmail.com>
2025-12-29 22:37:04 +0900

    build: myinvitaiton 모듈 paging, domain, ui, designsystem dependencies

--- android/feature/myinvitation/build.gradle.kts
+++ android/feature/myinvitation/build.gradle.kts
@@ -7,6 +7,21 @@ android {
 }
 
 dependencies {
+    // designSystem
+    implementation(projects.core.designsystem)
+
+    // ui
+    implementation(projects.core.ui)
+
+    // domain
+    implementation(projects.domain)
+
+    // coil
+    implementation(libs.coil.kt.compose)
+
+    // paging
+    implementation(libs.androidx.paging.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
