9ccb8b3b1753f6276cfda15d5b218a19237c4864
ant5555 <ymo6886@gmail.com>
2025-12-30 13:10:57 +0900

    feat: ui/util에 collectWithLifecycle 추가

---
+++ android/core/ui/src/main/kotlin/com/andlife/ui/util/FlowExtensions.kt
@@ -0,0 +1,23 @@
+package com.andlife.ui.util
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+
+@SuppressLint("ComposableNaming")
+@Composable
+fun <T> Flow<T>.collectWithLifecycle(
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    action: suspend (T) -> Unit
+) {
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    LaunchedEffect(this, lifecycle) {
+        lifecycle.repeatOnLifecycle(minActiveState) {
+            collect { action(it) }
+        }
+    }
+}
