8631bec5523f495f61e81f0f3f08514460058ec4
Donghyun <pdchoirian@gmail.com>
2025-12-30 19:48:42 +0900

    feat: Sample Service 구현

--- backend/src/main/kotlin/com/andlife/InvitationServer/service/SampleService.kt
+++ backend/src/main/kotlin/com/andlife/InvitationServer/service/SampleService.kt
@@ -1,4 +1,10 @@
 package com.andlife.InvitationServer.service
 
-class SampleService {
+import com.andlife.InvitationServer.entity.Sample
+import com.andlife.InvitationServer.repository.SampleRepository
+
+class SampleService(
+    private val sampleRepository: SampleRepository
+) {
+    fun getAllSamples(): List<Sample> = sampleRepository.findAll()
 }
