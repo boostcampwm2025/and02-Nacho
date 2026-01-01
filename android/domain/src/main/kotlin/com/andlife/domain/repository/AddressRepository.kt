265eac0d31dc7825b0085755051a6dab505714eb
ant5555 <ymo6886@gmail.com>
2026-01-01 22:39:52 +0900

    feat: SearchAddress Domain Layer

---
+++ android/domain/src/main/kotlin/com/andlife/domain/repository/AddressRepository.kt
@@ -0,0 +1,9 @@
+package com.andlife.domain.repository
+
+import androidx.paging.PagingData
+import com.andlife.domain.model.Address
+import kotlinx.coroutines.flow.Flow
+
+interface AddressRepository {
+    fun searchAddress(query: String): Flow<PagingData<Address>>
+}
