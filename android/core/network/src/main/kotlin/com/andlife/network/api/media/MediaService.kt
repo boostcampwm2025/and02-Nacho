0923e3d746f14e9661fd0013d1b86e1298f12f10
seojw0124 <wjddn2000124@gmail.com>
2026-01-03 23:34:52 +0900

    feat: MediaService 구현

--- android/core/network/src/main/kotlin/com/andlife/network/api/media/MediaService.kt
+++ android/core/network/src/main/kotlin/com/andlife/network/api/media/MediaService.kt
@@ -1,3 +1,12 @@
 package com.andlife.network.api.media
 
-interface MediaService
+import com.andlife.network.model.BaseResponse
+import retrofit2.http.POST
+
+interface MediaService {
+    @POST("/api/v1/media/start")
+    suspend fun uploadMedia(): BaseResponse<String>
+
+    @POST("/api/v1/media/complete")
+    suspend fun completeUploadMedia(): BaseResponse<Unit>
+}
