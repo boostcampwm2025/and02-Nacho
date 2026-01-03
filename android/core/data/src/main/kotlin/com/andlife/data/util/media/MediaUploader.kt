96d8de15de3b7ba962d9080d306ab349759883f5
seojw0124 <wjddn2000124@gmail.com>
2026-01-04 02:26:22 +0900

    feat: MediaUploader 구현

---
+++ android/core/data/src/main/kotlin/com/andlife/data/util/media/MediaUploader.kt
@@ -0,0 +1,19 @@
+package com.andlife.data.util.media
+
+import com.andlife.domain.error.DataError
+import com.andlife.domain.repository.MediaType
+import com.andlife.domain.util.Result
+import java.io.File
+
+interface MediaUploader {
+
+    suspend fun uploadMedia(
+        file: File,
+        mediaType: MediaType
+    ): Result<String, DataError>
+
+    suspend fun uploadMediaBatch(
+        files: List<Pair<File, MediaType>> // TODO: Pair -> Data Class
+    ): Result<List<String>, DataError>
+}
+
