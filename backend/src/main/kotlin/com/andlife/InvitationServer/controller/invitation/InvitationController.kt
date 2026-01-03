1db43c35d474511802a6ea6e45e56759e3a2bbaa
pdchoirian <pdchoirian@gmail.com>
2026-01-03 20:33:19 +0900

    feat: InvitationController Media 반환 api 구현

--- backend/src/main/kotlin/com/andlife/InvitationServer/controller/invitation/InvitationController.kt
+++ backend/src/main/kotlin/com/andlife/InvitationServer/controller/invitation/InvitationController.kt
@@ -1,9 +1,25 @@
 package com.andlife.InvitationServer.controller.invitation
 
+import com.andlife.InvitationServer.response.BaseResponse
+import com.andlife.InvitationServer.response.invitation.guestbook.CollectionResponse
+import com.andlife.InvitationServer.service.invitation.guestbook.GuestBookService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/invitations")
-class InvitationController {
+class InvitationController(
+    private val guestBookService: GuestBookService
+) {
+
+    @GetMapping("/{invitationId}/collection")
+    fun getMediaCollection(
+        @PathVariable invitationId: Long
+    ): BaseResponse<List<CollectionResponse>> {
+        val result = guestBookService.getCollectionByInvitation(invitationId)
+        return BaseResponse.success(result)
+    }
+
 }
