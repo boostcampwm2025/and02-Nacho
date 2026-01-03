2e6b16cdcb2bab9a652cd640383ed03539ea185c
pdchoirian <pdchoirian@gmail.com>
2026-01-03 19:16:59 +0900

    feat: 방명록 미디어 모아보기 Fetch Join 쿼리 추가

--- backend/src/main/kotlin/com/andlife/InvitationServer/repository/invitation/guestbook/GuestBookRepository.kt
+++ backend/src/main/kotlin/com/andlife/InvitationServer/repository/invitation/guestbook/GuestBookRepository.kt
@@ -2,5 +2,15 @@ package com.andlife.InvitationServer.repository.invitation.guestbook
 
 import com.andlife.InvitationServer.entity.GuestBook
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
-interface GuestBookRepository: JpaRepository<GuestBook, Long>
+interface GuestBookRepository: JpaRepository<GuestBook, Long> {
+    @Query("SELECT DISTINCT g FROM GuestBook g " +
+            "JOIN FETCH g.user " +
+            "LEFT JOIN FETCH g.images " +
+            "LEFT JOIN FETCH g.audios " +
+            "LEFT JOIN FETCH g.videos " +
+            "WHERE g.invitation.id = :invitationId")
+    fun findAllByInvitationIdWithDetails(@Param("invitationId") invitationId: Long): List<GuestBook>
+}
