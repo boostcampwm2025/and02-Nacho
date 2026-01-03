d28a6289d7a4bfb44247d62f42aa5af7be33e198
pdchoirian <pdchoirian@gmail.com>
2026-01-03 21:41:51 +0900

    chore: Fetch Join이 아닌 Betch Size를 사용하는 형태로 수정

--- backend/src/main/kotlin/com/andlife/InvitationServer/repository/invitation/guestbook/GuestBookRepository.kt
+++ backend/src/main/kotlin/com/andlife/InvitationServer/repository/invitation/guestbook/GuestBookRepository.kt
@@ -5,12 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
-interface GuestBookRepository: JpaRepository<GuestBook, Long> {
+interface GuestBookRepository : JpaRepository<GuestBook, Long> {
     @Query("SELECT DISTINCT g FROM GuestBook g " +
             "JOIN FETCH g.user " +
-            "LEFT JOIN FETCH g.images " +
-            "LEFT JOIN FETCH g.audios " +
-            "LEFT JOIN FETCH g.videos " +
             "WHERE g.invitation.id = :invitationId")
     fun findAllByInvitationIdWithDetails(@Param("invitationId") invitationId: Long): List<GuestBook>
 }
