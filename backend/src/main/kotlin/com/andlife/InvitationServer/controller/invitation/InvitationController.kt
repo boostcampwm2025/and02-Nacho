85786151f67ef751c3b58542aa960bde7cb2afb4
pdchoirian <pdchoirian@gmail.com>
2026-01-03 16:50:52 +0900

    feat: InvitationController 생성

---
+++ backend/src/main/kotlin/com/andlife/InvitationServer/controller/invitation/InvitationController.kt
@@ -0,0 +1,9 @@
+package com.andlife.InvitationServer.controller.invitation
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/invitations")
+class InvitationController {
+}
