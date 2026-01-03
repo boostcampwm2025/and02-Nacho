2d1560baafba3d98cec93446c16bab47c0dc8309
seojw0124 <wjddn2000124@gmail.com>
2026-01-03 23:24:49 +0900

    feat: 서버용 네트워크 모듈 구현

---
+++ android/core/network/src/main/kotlin/com/andlife/network/api/media/MediaService.kt
@@ -0,0 +1,4 @@
+package com.andlife.network.api.media
+
+interface MediaService {
+}
---
+++ android/core/network/src/main/kotlin/com/andlife/network/di/InvitationNetworkModule.kt
@@ -0,0 +1,53 @@
+package com.andlife.network.di
+
+import com.andlife.network.BuildConfig
+import com.andlife.network.api.media.MediaService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object InvitationNetworkModule {
+    private const val SERVER_BASE_URL = BuildConfig.SERVER_URL
+
+    @Provides
+    @Singleton
+    @InvitationOkHttp
+    fun provideInvitationOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+    @Provides
+    @Singleton
+    @InvitationRetrofit
+    fun provideInvitationRetrofit(
+        json: Json,
+        @InvitationOkHttp okHttpClient: OkHttpClient,
+    ): Retrofit =
+        Retrofit
+            .Builder()
+            .baseUrl(SERVER_BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideInvitationApiService(
+        @InvitationRetrofit retrofit: Retrofit,
+    ): MediaService =
+        retrofit.create(MediaService::class.java)
+}
