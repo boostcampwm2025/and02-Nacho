15fa85a98e8b9cc9c872cffd65696b2e4658bc87
pdchoirian <pdchoirian@gmail.com>
2026-01-03 16:50:38 +0900

    feat: CollectionResponse 정의

---
+++ backend/src/main/kotlin/com/andlife/InvitationServer/response/invitation/CollectionResponse.kt
@@ -0,0 +1,14 @@
+package com.andlife.InvitationServer.response.invitation
+
+import com.andlife.InvitationServer.constant.MediaType
+import com.andlife.InvitationServer.response.AuthorResponse
+import java.time.LocalDateTime
+
+data class CollectionResponse(
+    val id: Long,
+    val mediaType: MediaType,
+    val mediaUrl: String,
+    val author: AuthorResponse,
+    val createdAt: LocalDateTime,
+    val durationSeconds: Int? = null
+)
