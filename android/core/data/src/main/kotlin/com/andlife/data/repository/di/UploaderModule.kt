4960afcbef888c322876945f468494e6c57301a8
seojw0124 <wjddn2000124@gmail.com>
2026-01-04 03:30:09 +0900

    chore: Uploader 의존성 주입 Provides -> Binds

--- android/core/data/src/main/kotlin/com/andlife/data/repository/di/UploaderModule.kt
+++ android/core/data/src/main/kotlin/com/andlife/data/repository/di/UploaderModule.kt
@@ -2,25 +2,17 @@ package com.andlife.data.repository.di
 
 import com.andlife.data.util.media.MediaUploader
 import com.andlife.data.util.media.MediaUploaderImpl
-import com.andlife.network.api.media.MediaService
-import com.andlife.network.di.MediaOkHttp
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import jakarta.inject.Singleton
-import okhttp3.OkHttpClient
 
 @Module
 @InstallIn(SingletonComponent::class)
-object UploaderModule {
+abstract class UploaderModule {
 
-    @Provides // TODO: Binds
+    @Binds
     @Singleton
-    fun provideMediaUploader(
-        mediaService: MediaService,
-        @MediaOkHttp okHttpClient: OkHttpClient
-    ): MediaUploader {
-        return MediaUploaderImpl(mediaService, okHttpClient)
-    }
+    abstract fun bindMediaUploader(impl: MediaUploaderImpl): MediaUploader
 }
