261f0aa7b80774e95799383ab1ef1d64f3271954
ant5555 <ymo6886@gmail.com>
2026-01-03 23:59:29 +0900

    refactor: Meta와 Document 중첩 클래스로 변경

--- android/core/network/src/main/kotlin/com/andlife/network/api/kakao/address/KakaoAddressResponse.kt
+++ android/core/network/src/main/kotlin/com/andlife/network/api/kakao/address/KakaoAddressResponse.kt
@@ -7,21 +7,21 @@ import kotlinx.serialization.Serializable
 data class KakaoAddressResponse(
     @SerialName("meta") val meta: Meta,
     @SerialName("documents") val documents: List<Document>,
-)
+) {
+    @Serializable
+    data class Meta(
+        @SerialName("total_count") val totalCount: Int,
+        @SerialName("pageable_count") val pageableCount: Int,
+        @SerialName("is_end") val isEnd: Boolean,
+    )
 
-@Serializable
-data class Meta(
-    @SerialName("total_count") val totalCount: Int,
-    @SerialName("pageable_count") val pageableCount: Int,
-    @SerialName("is_end") val isEnd: Boolean,
-)
-
-@Serializable
-data class Document(
-    @SerialName("id") val id: String,
-    @SerialName("place_name") val placeName: String,
-    @SerialName("address_name") val addressName: String,
-    @SerialName("road_address_name") val roadAddressName: String,
-    @SerialName("x") val longitude: String,
-    @SerialName("y") val latitude: String,
-)
+    @Serializable
+    data class Document(
+        @SerialName("id") val id: String,
+        @SerialName("place_name") val placeName: String,
+        @SerialName("address_name") val addressName: String,
+        @SerialName("road_address_name") val roadAddressName: String,
+        @SerialName("x") val longitude: String,
+        @SerialName("y") val latitude: String,
+    )
+}
