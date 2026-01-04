d3801116615b263d1296346be5280b696d81c3e1
jm3789 <jm37890@gmail.com>
2026-01-04 20:33:44 +0900

    build: core/ui에 viewModelCompose 의존성 추가

--- android/core/ui/build.gradle.kts
+++ android/core/ui/build.gradle.kts
@@ -20,6 +20,9 @@ dependencies {
     // paging
     implementation(libs.androidx.paging.compose)
 
+    // lifecycle
+    implementation(libs.androidx.lifecycle.viewModelCompose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
