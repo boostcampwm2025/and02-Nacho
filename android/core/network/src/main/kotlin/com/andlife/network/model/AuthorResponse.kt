8495cc504b91df12434a9b6e07c37ef5318b9cf2
pdchoirian <pdchoirian@gmail.com>
2026-01-04 17:47:39 +0900

    feat: AuthorResponse 정의

---
+++ android/core/network/src/main/kotlin/com/andlife/network/model/AuthorResponse.kt
@@ -0,0 +1,11 @@
+package com.andlife.network.model
+
+import kotlinx.serialization.Serializable
+
+@OptIn(kotlinx.serialization.InternalSerializationApi::class)
+@Serializable
+data class AuthorResponse(
+    val userId: Long,
+    val name: String,
+    val profileImageUrl: String? = null
+)
