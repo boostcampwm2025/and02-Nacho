85340836591c29609ecbdf2f3a8b4a6da55e2baf
pdchoirian <pdchoirian@gmail.com>
2026-01-05 20:46:50 +0900

    chore: 임시 파일 삭제, 코틀린 datetime 라이브러리 추가

--- android/feature/invitation/build.gradle.kts
+++ android/feature/invitation/build.gradle.kts
@@ -13,6 +13,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    // kotlinx datatime
+    implementation(libs.kotlinx.datetime)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
