4bacb0321917fd306cd41dd7111c37c583178e24
seojw0124 <wjddn2000124@gmail.com>
2026-01-05 23:40:42 +0900

    chore: Ktlint

--- android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/guestbook/GuestBookMapper.kt
+++ android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/guestbook/GuestBookMapper.kt
@@ -6,8 +6,8 @@ import com.andlife.domain.model.MediaType
 import com.andlife.network.api.guestbook.GuestBookEntryMediaResponse
 import com.andlife.network.api.guestbook.GuestBookResponse
 
-fun GuestBookResponse.toDomain(): GuestBook {
-    return GuestBook(
+fun GuestBookResponse.toDomain(): GuestBook =
+    GuestBook(
         id = id,
         authorName = authorName,
         authorProfileImage = authorProfileImage,
@@ -18,14 +18,12 @@ fun GuestBookResponse.toDomain(): GuestBook {
         totalVisualCount = totalVisualCount,
         createdAt = createdAt,
     )
-}
 
-fun GuestBookEntryMediaResponse.toDomain(): GuestBookEntryMedia {
-    return GuestBookEntryMedia(
+fun GuestBookEntryMediaResponse.toDomain(): GuestBookEntryMedia =
+    GuestBookEntryMedia(
         type = MediaType.fromString(type),
         url = url,
         thumbnailUrl = thumbnailUrl,
         durationSeconds = durationSeconds,
-        displayOrder = displayOrder
+        displayOrder = displayOrder,
     )
-}
