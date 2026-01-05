f0e004dcca4b2c56b93520cc8e9ac5092df19f84
pdchoirian <pdchoirian@gmail.com>
2026-01-05 13:58:12 +0900

    chore: data Module kotlinx datatime 라이브러리 추가

--- android/core/data/build.gradle.kts
+++ android/core/data/build.gradle.kts
@@ -25,4 +25,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // kotlinx datetime
+    implementation(libs.kotlinx.datetime)
 }
