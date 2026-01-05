a2d45f888450b60f837889ecbc3389476e7b5f04
pdchoirian <pdchoirian@gmail.com>
2026-01-05 17:51:39 +0900

    feat: RemoteDataSourceModule DI 모듈 추가

---
+++ android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/di/GuestBookRemoteDataSourceModule.kt
@@ -0,0 +1,21 @@
+package com.andlife.data.datasource.remote.di
+
+import com.andlife.data.datasource.remote.invitation.guestbook.GuestBookRemoteDataSource
+import com.andlife.data.datasource.remote.invitation.guestbook.GuestBookRemoteDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class GuestBookRemoteDataSourceModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindGuestBookRemoteDataSource(
+        guestBookRemoteDataSourceImpl: GuestBookRemoteDataSourceImpl
+    ): GuestBookRemoteDataSource
+
+}
