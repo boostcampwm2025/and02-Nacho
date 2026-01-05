2da3151d038cc2ad8be963214707251abe2e5837
pdchoirian <pdchoirian@gmail.com>
2026-01-05 14:07:50 +0900

    feat: GuestBookService 모아보기 API를 호출하는 함수 구현

---
+++ android/core/network/src/main/kotlin/com/andlife/network/api/guestbook/GuestBookService.kt
@@ -0,0 +1,15 @@
+package com.andlife.network.api.guestbook
+
+import com.andlife.network.model.BaseResponse
+import com.andlife.network.model.invitation.guestbook.CollectionResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GuestBookService {
+
+    @GET("/api/invitations/{invitationId}/collection")
+    suspend fun getMediaCollection(
+        @Path("invitationId") invitationId: Long
+    ): BaseResponse<List<CollectionResponse>>
+
+}
