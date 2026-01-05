4317ff7287360e02e04c810210455ad7bd353ff1
pdchoirian <pdchoirian@gmail.com>
2026-01-05 17:53:54 +0900

    feat: InvitationScreen에 GridView 보여주도록 임시로 구현

--- android/feature/invitation/src/main/kotlin/com/andlife/invitation/screen/InvitationScreen.kt
+++ android/feature/invitation/src/main/kotlin/com/andlife/invitation/screen/InvitationScreen.kt
@@ -1,10 +1,22 @@
 package com.andlife.invitation.screen
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.andlife.invitation.viewmodel.InvitationViewModel
+import com.andlife.ui.component.media.MediaGridView
 
 @Composable
-fun InvitationScreen(modifier: Modifier = Modifier) {
-    Text(text = "InvitationScreen")
+fun InvitationScreen(
+    modifier: Modifier = Modifier,
+    viewModel: InvitationViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    MediaGridView(
+        uiState.mediaItems,
+        onItemClick = {}
+    )
 }
