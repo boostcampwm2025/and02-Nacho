e3396e0c7964ee0860468cc3f6022d0c97a27c0b
pdchoirian <pdchoirian@gmail.com>
2026-01-05 13:42:50 +0900

    feat: String -> MediaType으로 변환해주는 함수 구현

--- android/domain/src/main/kotlin/com/andlife/domain/model/MediaType.kt
+++ android/domain/src/main/kotlin/com/andlife/domain/model/MediaType.kt
@@ -2,4 +2,14 @@ package com.andlife.domain.model
 
 enum class MediaType {
     IMAGE, VIDEO, AUDIO;
+
+    companion object {
+        fun from(type: String?): MediaType {
+            return when (type?.uppercase()) {
+                "VIDEO" -> VIDEO
+                "AUDIO" -> AUDIO
+                else -> IMAGE
+            }
+        }
+    }
 }
