ca9ac2147b0e33550294a4638607974ff3fe0229
pdchoirian <pdchoirian@gmail.com>
2026-01-06 18:20:01 +0900

    chore: 모듈 의존성 수정

--- android/core/network/build.gradle.kts
+++ android/core/network/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("invitation.android.library")
     id("invitation.kotlin.serialization")
@@ -6,6 +8,20 @@ plugins {
 
 android {
     namespace = "com.andlife.network"
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+
+        val properties = Properties()
+        val localProps = project.rootProject.file("local.properties")
+        properties.load(localProps.inputStream())
+        val serverUrl = properties.getProperty("SERVER_URL") ?: ""
+
+        buildConfigField("String", "SERVER_URL", "\"$serverUrl\"")
+    }
 }
 
 dependencies {
@@ -14,9 +30,6 @@ dependencies {
     implementation(libs.retrofit.kotlin.serialization)
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
-
-    // kotlin datetime
-    implementation(libs.kotlinx.datetime)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
