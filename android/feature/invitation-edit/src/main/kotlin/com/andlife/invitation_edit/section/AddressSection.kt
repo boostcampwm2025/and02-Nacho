4014f1d5e927bd870634ac80aa4659a393202fd2
kim dong kyung <kdkd422@naver.com>
2026-01-07 06:09:16 +0900

    feat: 장소 섹션

---
+++ android/feature/invitation-edit/src/main/kotlin/com/andlife/invitation_edit/section/AddressSection.kt
@@ -0,0 +1,81 @@
+package com.andlife.invitation_edit.section
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.andlife.designsystem.component.InvitationTextField
+import com.andlife.designsystem.preview.PreviewTheme
+import com.andlife.designsystem.theme.InvitationSpacing
+import com.andlife.designsystem.theme.InvitationTheme
+import com.andlife.invitation_edit.R
+import com.andlife.invitation_edit.component.FormLabel
+
+@Composable
+internal fun AddressSection(
+    placeName: String,
+    placeAddress: String,
+    addressGuide: String,
+    onChangePlaceAddress: (String) -> Unit,
+    onChangeAddressGuide: (String) -> Unit,
+    onNavigateToAddressSearch: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier.background(InvitationTheme.colorScheme.backgroundPrimary)) {
+        Column(
+            modifier = Modifier.padding(InvitationSpacing.large),
+            verticalArrangement = Arrangement.spacedBy(InvitationSpacing.small),
+        ) {
+            FormLabel(title = stringResource(R.string.txt_invitation_address))
+            InvitationTextField(
+                value = placeName,
+                onValueChange = {},
+                enabled = false,
+                readOnly = true,
+                placeholder = stringResource(R.string.txt_address),
+                trailingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = stringResource(R.string.desc_address),
+                        tint = InvitationTheme.colorScheme.brandPrimary,
+                    )
+                },
+                modifier = Modifier.clickable { onNavigateToAddressSearch() },
+            )
+            InvitationTextField(
+                value = placeAddress,
+                onValueChange = onChangePlaceAddress,
+                placeholder = stringResource(R.string.txt_address_detail),
+            )
+            InvitationTextField(
+                value = addressGuide,
+                onValueChange = onChangeAddressGuide,
+                placeholder = stringResource(R.string.txt_guide_address),
+                singleLine = false,
+            )
+        }
+    }
+}
+
+@Composable
+@PreviewTheme
+private fun AddressSectionPreview() {
+    InvitationTheme {
+        AddressSection(
+            placeName = "코드스쿼드",
+            placeAddress = "서울특별시 강남구 강남대로62길 23",
+            addressGuide = "오시는 길을 입력해주세요.",
+            onChangePlaceAddress = {},
+            onChangeAddressGuide = {},
+            onNavigateToAddressSearch = {},
+        )
+    }
+}
