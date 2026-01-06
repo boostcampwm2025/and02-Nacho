18cb99b2a7f094451c4c5914a7fe91f4709f296d
ant5555 <ymo6886@gmail.com>
2026-01-07 00:54:28 +0900

    feat: DeferredDeepLink 구현

--- android/app/src/main/kotlin/com/andlife/invitationzzang/InvitationApp.kt
+++ android/app/src/main/kotlin/com/andlife/invitationzzang/InvitationApp.kt
@@ -1,15 +1,27 @@
 package com.andlife.invitationzzang
 
+import android.content.Intent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import com.andlife.invitationzzang.navigation.InvitationNavHost
 import com.andlife.invitationzzang.navigation.rememberInvitationNavigator
 
 @Composable
 fun InvitationApp(
+    deepLinkIntent: Intent? = null,
     modifier: Modifier = Modifier
 ) {
     val navigator = rememberInvitationNavigator()
+
+    LaunchedEffect(deepLinkIntent) {
+        deepLinkIntent?.let { intent ->
+            if (intent.data != null) {
+                navigator.navController.handleDeepLink(intent)
+            }
+        }
+    }
+
     InvitationNavHost(
         navigator = navigator,
         modifier = modifier,
--- android/app/src/main/kotlin/com/andlife/invitationzzang/MainActivity.kt
+++ android/app/src/main/kotlin/com/andlife/invitationzzang/MainActivity.kt
@@ -1,31 +1,62 @@
 package com.andlife.invitationzzang
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.andlife.designsystem.theme.InvitationTheme
+import com.andlife.invitationzzang.deeplink.InstallReferrerHandler
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private var currentIntent by mutableStateOf<Intent?>(null)
+    private var installReferrerHandler: InstallReferrerHandler? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("MainActivity", "onCreate called, intent: ${intent?.data}")
+
+        if (savedInstanceState == null && intent.data == null) {
+            checkDeferredDeepLink()
+        } else {
+            currentIntent = intent
+        }
+
         enableEdgeToEdge()
         setContent {
             InvitationTheme {
-                InvitationApp()
+                InvitationApp(
+                    deepLinkIntent = currentIntent
+                )
             }
         }
+    }
+
+    private fun checkDeferredDeepLink() {
+        installReferrerHandler = InstallReferrerHandler(this) { invitationId ->
+            Log.d("DeferredDeepLink", "Received invitation ID from Install Referrer: $invitationId")
+
+            val deepLinkUri = Uri.parse("https://invitationzzang.com/invite/$invitationId")
+            val deepLinkIntent = Intent(Intent.ACTION_VIEW, deepLinkUri)
+
+            currentIntent = deepLinkIntent
+        }
+        installReferrerHandler?.startConnection()
     }
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         Log.d("MainActivity", "onNewIntent called, intent: ${intent.data}")
         setIntent(intent)
+
+        currentIntent = intent
     }
 
     override fun onStart() {
