008ef3b5a97eb7b0f7f1bcf569e7291436c74458
pdchoirian <pdchoirian@gmail.com>
2026-01-06 19:34:07 +0900

    chore: 방명록 모아보기 관련 파일들에 Collection 이름을 붙여 구분이 쉽게 변경

--- android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/di/RemoteDataSourceModule.kt
+++ android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/di/RemoteDataSourceModule.kt
@@ -12,7 +12,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal abstract class AddressRemoteDataSourceModule {
+internal abstract class RemoteDataSourceModule {
     @Binds
     @Singleton
     abstract fun bindAddressRemoteDataSource(
