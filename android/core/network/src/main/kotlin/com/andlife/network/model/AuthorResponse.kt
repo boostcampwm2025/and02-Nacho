3ea2554a736b825a2b63ea75f045e24886ba602b
pdchoirian <pdchoirian@gmail.com>
2026-01-06 18:26:00 +0900

    chore: AuthorResponse userId → id로 변경

--- android/core/network/src/main/kotlin/com/andlife/network/model/AuthorResponse.kt
+++ android/core/network/src/main/kotlin/com/andlife/network/model/AuthorResponse.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 @OptIn(kotlinx.serialization.InternalSerializationApi::class)
 @Serializable
 data class AuthorResponse(
-    val userId: Long,
+    val id: Long,
     val name: String,
     val profileImageUrl: String? = null
 )
