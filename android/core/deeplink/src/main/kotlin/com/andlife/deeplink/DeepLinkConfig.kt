35e8611009194e832472480929e0f30685eb3488
ant5555 <ymo6886@gmail.com>
2026-01-10 15:11:40 +0900

    chore: 불필요한 주석 제거

--- android/core/deeplink/src/main/kotlin/com/andlife/deeplink/DeepLinkConfig.kt
+++ android/core/deeplink/src/main/kotlin/com/andlife/deeplink/DeepLinkConfig.kt
@@ -12,45 +12,24 @@ object DeepLinkConfig {
     const val KAKAO_HOST = "kakaolink"
     const val PARAM_INVITE_ID = "invite_id"
 
-    /**
-     * 웹 딥링크 베이스 URL
-     * @return https://invitationzzang.com/invite
-     */
-    fun buildWebDeepLinkBase(): String = "$SCHEME://$HOST/$PATH_INVITE/{id}"
+    fun buildWebDeepLinkBase(): String = "$SCHEME://$HOST/$PATH_INVITE"
 
-    /**
-     * 특정 초대장의 웹 딥링크
-     * @param invitationId 초대장 ID
-     * @return https://invitationzzang.com/invite/123
-     */
+    fun buildKakaoDeepLinkPattern(kakaoKey: String): String {
+        return "${KAKAO_SCHEME_PREFIX}${kakaoKey}://${KAKAO_HOST}?${PARAM_INVITE_ID}={id}"
+    }
+
     fun buildInvitationDeepLink(invitationId: Long): String {
         return "${buildWebDeepLinkBase()}/$invitationId"
     }
 
-    /**
-     * Uri 객체로 변환
-     */
     fun buildInvitationDeepLinkUri(invitationId: Long): Uri {
         return buildInvitationDeepLink(invitationId).toUri()
     }
 
-    /**
-     * Play Store referrer URL (Deferred DeepLink용)
-     * TODO: @param invitationId 초대장 ID
-     * @return Play Store URL with referrer parameter
-     */
+    // TODO: Deferred Deep Link를 위한 referrer 포함 URL
     fun buildPlayStoreUrl(invitationId: Long): String {
         return "https://play.google.com/store/apps/details?" +
             "id=com.andlife.invitationzzang&" +
             "referrer=invite_id%3D$invitationId"
-    }
-
-    /**
-     * Kakao 딥링크 패턴 (Navigation용)
-     * @param kakaoKey Kakao Native App Key
-     * @return kakao{key}://kakaolink?invite_id={id}
-     */
-    fun buildKakaoDeepLinkPattern(kakaoKey: String): String {
-        return "$KAKAO_SCHEME_PREFIX$kakaoKey://$KAKAO_HOST?$PARAM_INVITE_ID={id}"
     }
 }
