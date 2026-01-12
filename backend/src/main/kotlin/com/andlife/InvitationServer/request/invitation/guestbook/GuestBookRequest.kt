f033fe7a1241c59675d499290ace41065baac031
jm3789 <jm37890@gmail.com>
2026-01-13 01:59:03 +0900

    feat: 서버에 GuestBook, Media에 대한 request 추가

---
+++ backend/src/main/kotlin/com/andlife/InvitationServer/request/invitation/guestbook/GuestBookRequest.kt
@@ -0,0 +1,16 @@
+package com.andlife.InvitationServer.request.invitation.guestbook
+
+data class GuestBookRequest(
+    val invitationId: Long,
+    val userId: Long,
+    val textContent: String,
+    val medias: List<GuestBookMediaRequest> = emptyList()
+)
+
+data class GuestBookMediaRequest(
+    val mediaType: String,
+    val mediaUrl: String,
+    val durationSeconds: Int? = null, // 오디오, 비디오인 경우에만 사용
+    val thumbnailUrl: String? = null, // 비디오인 경우에만 사용
+    val displayOrder: Int,
+)
