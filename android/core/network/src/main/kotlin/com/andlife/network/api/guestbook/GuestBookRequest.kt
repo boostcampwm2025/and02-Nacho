72cb968414dbc86b0689ff23cacf268ac9bddc55
jm3789 <jm37890@gmail.com>
2026-01-13 02:55:31 +0900

    feat: core:network 에 방명록 request 객체 추가

---
+++ android/core/network/src/main/kotlin/com/andlife/network/api/guestbook/GuestBookRequest.kt
@@ -0,0 +1,23 @@
+@file:OptIn(InternalSerializationApi::class)
+
+package com.andlife.network.api.guestbook
+
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GuestBookRequest(
+    val invitationId: Long,
+    val userId: Long,
+    val textContent: String,
+    val medias: List<GuestBookMediaRequest> = emptyList(),
+)
+
+@Serializable
+data class GuestBookMediaRequest(
+    val mediaType: String,
+    val mediaUrl: String,
+    val durationSeconds: Int? = null, // 오디오, 비디오인 경우에만 사용
+    val thumbnailUrl: String? = null, // 비디오인 경우에만 사용
+    val displayOrder: Int,
+)
