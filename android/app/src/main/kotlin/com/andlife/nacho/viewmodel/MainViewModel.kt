62a3cf2c94fa653bf9bf4be245df2a64ddebc649
ant5555 <ymo6886@gmail.com>
2026-01-13 21:51:20 +0900

    feat: asStateFlow() 명시

--- android/app/src/main/kotlin/com/andlife/nacho/viewmodel/MainViewModel.kt
+++ android/app/src/main/kotlin/com/andlife/nacho/viewmodel/MainViewModel.kt
@@ -3,12 +3,14 @@ package com.andlife.nacho.viewmodel
 import android.content.Intent
 import androidx.lifecycle.viewModelScope
 import com.andlife.deeplink.DeepLinkManager
+import com.andlife.invitation_edit.model.create.CreateInvitationUiState
 import com.andlife.nacho.model.MainSideEffect
 import com.andlife.nacho.model.MainUiEvent
 import com.andlife.nacho.model.MainUiState
 import com.andlife.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.launchIn
@@ -23,7 +25,7 @@ class MainViewModel @Inject constructor(
 ) {
     private var lastProcessedId: String? = null
 
-    override val uiState: StateFlow<MainUiState> = mutableUiState
+    override val uiState: StateFlow<MainUiState> = mutableUiState.asStateFlow()
 
     override fun onEvent(event: MainUiEvent) {
         // No events yet
