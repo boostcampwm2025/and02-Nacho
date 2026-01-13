fa595a2ff0dbfb996de61459e9d81648820cc1fe
ant5555 <ymo6886@gmail.com>
2026-01-13 20:56:06 +0900

    refactor: MyInvitationDetailUiEvent sealed interface 변경

--- android/feature/myinvitation/src/main/kotlin/com/andlife/myinvitation/model/MyInvitationDetailUiEvent.kt
+++ android/feature/myinvitation/src/main/kotlin/com/andlife/myinvitation/model/MyInvitationDetailUiEvent.kt
@@ -2,8 +2,8 @@ package com.andlife.myinvitation.model
 
 import com.andlife.ui.base.BaseUiEvent
 
-sealed class MyInvitationDetailUiEvent : BaseUiEvent {
-    data object ClickBack : MyInvitationDetailUiEvent()
+sealed interface MyInvitationDetailUiEvent : BaseUiEvent {
+    data object ClickBack : MyInvitationDetailUiEvent
 
-    data object ClickShare : MyInvitationDetailUiEvent()
+    data object ClickShare : MyInvitationDetailUiEvent
 }
