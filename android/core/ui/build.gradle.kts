38bd3ca979fde944d568697db53933038e4a47b1
ant5555 <ymo6886@gmail.com>
2026-01-14 23:51:30 +0900

    build: naver map sdk 추가

--- android/core/ui/build.gradle.kts
+++ android/core/ui/build.gradle.kts
@@ -29,6 +29,9 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewModelCompose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
 
+    // Naver Map
+    implementation(libs.naver.map.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
