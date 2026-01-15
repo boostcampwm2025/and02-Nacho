b2bfb4b46830f448d144008d4420c97890b8c8f3
jm3789 <jm37890@gmail.com>
2026-01-15 21:05:06 +0900

    chore: 필요없는 import 제거

--- android/core/ui/src/main/kotlin/com/andlife/ui/component/invitation/InvitationGuestBookForm.kt
+++ android/core/ui/src/main/kotlin/com/andlife/ui/component/invitation/InvitationGuestBookForm.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
@@ -101,7 +99,7 @@ fun InvitationGuestBookForm(
                         .padding(NachoSpacing.small),
             )
         }
-        
+
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
