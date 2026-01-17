e7e7500103fd1ed712e568c1a03ca463cd2b80ad
ant5555 <ymo6886@gmail.com>
2026-01-17 21:47:08 +0900

    feat: InvitationController에 invitationService 추가 (Backend)

--- backend/src/main/kotlin/com/andlife/InvitationServer/controller/invitation/InvitationController.kt
+++ backend/src/main/kotlin/com/andlife/InvitationServer/controller/invitation/InvitationController.kt
@@ -1,8 +1,10 @@
 package com.andlife.InvitationServer.controller.invitation
 
 import com.andlife.InvitationServer.response.BaseResponse
+import com.andlife.InvitationServer.response.invitation.InvitationResponse
 import com.andlife.InvitationServer.response.invitation.guestbook.CollectionResponse
 import com.andlife.InvitationServer.response.invitation.guestbook.GuestBookResponse
+import com.andlife.InvitationServer.service.invitation.InvitationService
 import com.andlife.InvitationServer.service.invitation.guestbook.GuestBookService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,8 +14,17 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/invitations")
 class InvitationController(
-    private val guestBookService: GuestBookService
+    private val invitationService: InvitationService,
+    private val guestBookService: GuestBookService,
 ) {
+
+    @GetMapping("/{invitationId}")
+    fun getInvitation(
+        @PathVariable invitationId: Long
+    ): BaseResponse<InvitationResponse> {
+        val result = invitationService.getInvitation(invitationId)
+        return BaseResponse.success(result)
+    }
 
     @GetMapping("/{invitationId}/collection")
     fun getMediaCollection(
