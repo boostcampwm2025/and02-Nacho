c2b7e716e5ebba89efa045fd6bd00daa0d7e87b6
ant5555 <ymo6886@gmail.com>
2026-01-17 21:49:01 +0900

    feat: 도메인 모듈 Invitation 모댈 및 InvitationRepository 인터페이스 작성

---
+++ android/domain/src/main/kotlin/com/andlife/domain/repository/invitation/InvitationRepository.kt
@@ -0,0 +1,10 @@
+package com.andlife.domain.repository.invitation
+
+import com.andlife.domain.error.DataError
+import com.andlife.domain.model.invitation.Invitation
+import com.andlife.domain.util.Result
+
+interface InvitationRepository {
+    suspend fun getInvitation(invitationId: Long): Result<Invitation, DataError>
+}
+
