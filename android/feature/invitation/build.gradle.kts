cf1ff81cb9a82c6cf05742fc99129887365b7cdd
seojw0124 <wjddn2000124@gmail.com>
2026-01-20 01:18:49 +0900

    build: feature:invitation에 Paging3 의존성 추가

--- android/feature/invitation/build.gradle.kts
+++ android/feature/invitation/build.gradle.kts
@@ -19,6 +19,9 @@ dependencies {
     // ExoPlayer
     implementation(libs.bundles.media3)
 
+    // Paging
+    implementation(libs.androidx.paging.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
