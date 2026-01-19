928044bc7c46280f52415c20790f1a02a2b7ad9a
seojw0124 <wjddn2000124@gmail.com>
2026-01-20 01:07:28 +0900

    feat: 방명록 리스트 가져오는 로직에 Paging 적용

--- android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/guestbook/GuestBookRemoteDataSource.kt
+++ android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/guestbook/GuestBookRemoteDataSource.kt
@@ -4,12 +4,17 @@ import com.andlife.domain.error.DataError
 import com.andlife.domain.util.Result
 import com.andlife.network.api.guestbook.GuestBookRequest
 import com.andlife.network.api.guestbook.GuestBookResponse
+import com.andlife.network.model.PagingResponse
 import com.andlife.network.model.invitation.guestbook.CollectionResponse
 
 interface GuestBookRemoteDataSource {
     suspend fun getMediaCollection(invitationId: Long): Result<List<CollectionResponse>, DataError>
 
-    suspend fun getGuestBooksByInvitationId(invitationId: Long): Result<List<GuestBookResponse>, DataError>
+    suspend fun getGuestBooksByInvitationId(
+        invitationId: Long,
+        page: Int,
+        size: Int,
+    ): Result<PagingResponse<GuestBookResponse>, DataError>
 
     suspend fun createGuestBook(invitationId: Long, request: GuestBookRequest): Result<GuestBookResponse, DataError>
 }
