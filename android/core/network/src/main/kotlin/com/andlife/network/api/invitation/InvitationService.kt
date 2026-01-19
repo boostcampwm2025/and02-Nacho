1cfd19ef69dad143234a2fc3cc8ca6c0f58b4604
kim dong kyung <kdkd422@naver.com>
2026-01-20 01:50:52 +0900

    feat: 초대생성 apiservice 작성

--- android/core/network/src/main/kotlin/com/andlife/network/api/invitation/InvitationService.kt
+++ android/core/network/src/main/kotlin/com/andlife/network/api/invitation/InvitationService.kt
@@ -1,7 +1,9 @@
 package com.andlife.network.api.invitation
 
 import com.andlife.network.model.BaseResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface InvitationService {
@@ -9,5 +11,10 @@ interface InvitationService {
     @GET("/api/invitations/{invitationId}")
     suspend fun getInvitation(
         @Path("invitationId") invitationId: Long,
+    ): BaseResponse<InvitationResponse>
+
+    @POST("/api/invitations")
+    suspend fun createInvitation(
+        @Body request: CreateInvitationRequest,
     ): BaseResponse<InvitationResponse>
 }
