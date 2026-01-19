6723dc423c9f55f1286432080fa3ddd14a40df07
seojw0124 <wjddn2000124@gmail.com>
2026-01-19 22:21:17 +0900

    feat: 방명록 리스트 api 페이징 추가

--- backend/src/main/kotlin/com/andlife/InvitationServer/controller/invitation/InvitationController.kt
+++ backend/src/main/kotlin/com/andlife/InvitationServer/controller/invitation/InvitationController.kt
@@ -3,11 +3,15 @@ package com.andlife.InvitationServer.controller.invitation
 import com.andlife.InvitationServer.request.invitation.guestbook.GuestBookRequest
 import com.andlife.InvitationServer.response.BaseResponse
 import com.andlife.InvitationServer.response.CommonResponseCode
+import com.andlife.InvitationServer.response.PagingResponse
 import com.andlife.InvitationServer.response.invitation.InvitationResponse
 import com.andlife.InvitationServer.response.invitation.guestbook.CollectionResponse
 import com.andlife.InvitationServer.response.invitation.guestbook.GuestBookResponse
 import com.andlife.InvitationServer.service.invitation.InvitationService
 import com.andlife.InvitationServer.service.invitation.guestbook.GuestBookService
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -39,9 +43,11 @@ class InvitationController(
     }
 
     @GetMapping("/{invitationId}/guestbooks")
-    fun getGuestBooks(@PathVariable invitationId: Long): BaseResponse<List<GuestBookResponse>> {
-        val result = guestBookService.getGuestBooks(invitationId)
-
+    fun getGuestBooks(
+        @PathVariable invitationId: Long,
+        @PageableDefault(size = 10, sort = ["createdAt"], direction = Sort.Direction.DESC) pageable: Pageable
+    ): BaseResponse<PagingResponse<GuestBookResponse>> {
+        val result = guestBookService.getGuestBooks(invitationId, pageable)
         return BaseResponse.success(result)
     }
 
@@ -63,6 +69,4 @@ class InvitationController(
             )
         }
     }
-
-
 }
--- backend/src/main/kotlin/com/andlife/InvitationServer/repository/invitation/guestbook/GuestBookRepository.kt
+++ backend/src/main/kotlin/com/andlife/InvitationServer/repository/invitation/guestbook/GuestBookRepository.kt
@@ -1,6 +1,8 @@
 package com.andlife.InvitationServer.repository.invitation.guestbook
 
 import com.andlife.InvitationServer.entity.GuestBook
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -14,8 +16,8 @@ interface GuestBookRepository : JpaRepository<GuestBook, Long> {
     @Query("""
         SELECT gb FROM GuestBook gb 
         JOIN FETCH gb.user
-        WHERE gb.invitation.id = :invitationId 
-        ORDER BY gb.createdAt DESC
+        JOIN FETCH gb.invitation
+        WHERE gb.invitation.id = :invitationId
     """)
-    fun findAllByInvitationId(invitationId: Long): List<GuestBook>
+    fun findAllByInvitationId(invitationId: Long, pageable: Pageable): Page<GuestBook>
 }
