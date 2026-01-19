d45d92705328cc5e6e8cd4c523f61b1289563807
seojw0124 <wjddn2000124@gmail.com>
2026-01-19 11:10:22 +0900

    chore: 불필요한 import 제거

--- android/core/ui/src/main/kotlin/com/andlife/ui/component/guestbook/GuestBookItem.kt
+++ android/core/ui/src/main/kotlin/com/andlife/ui/component/guestbook/GuestBookItem.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
