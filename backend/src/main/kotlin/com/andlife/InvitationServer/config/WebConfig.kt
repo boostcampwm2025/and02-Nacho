10592954229cd8f7caa82ea9ec622aa759540cf0
pdchoirian <pdchoirian@gmail.com>
2026-01-21 00:28:34 +0900

    chore: WebConfig 수정

--- backend/src/main/kotlin/com/andlife/InvitationServer/config/WebConfig.kt
+++ backend/src/main/kotlin/com/andlife/InvitationServer/config/WebConfig.kt
@@ -1,17 +1,17 @@
 package com.andlife.InvitationServer.config
 
-import com.andlife.InvitationServer.auth.AuthUserIdResolver
+import com.andlife.InvitationServer.auth.AuthContextResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
-    private val authUserIdResolver: AuthUserIdResolver
+    private val authContextResolver: AuthContextResolver
 ) : WebMvcConfigurer {
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
-        resolvers.add(authUserIdResolver)
+        resolvers.add(authContextResolver)
     }
 
 }
