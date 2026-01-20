96f91df8bf48129178cd697efd4d911475e8d6be
ant5555 <ymo6886@gmail.com>
2026-01-20 21:49:56 +0900

    feat: UpcomingInvitation

---
+++ android/domain/src/main/kotlin/com/andlife/domain/model/invitation/UpcomingInvitation.kt
@@ -0,0 +1,13 @@
+package com.andlife.domain.model.invitation
+
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalTime
+
+data class UpcomingInvitation(
+    val id: Long,
+    val title: String,
+    val thumbnailUrl: String?,
+    val invitationDate: LocalDate,
+    val startTime: LocalTime,
+    val displayHostName: String,
+)
