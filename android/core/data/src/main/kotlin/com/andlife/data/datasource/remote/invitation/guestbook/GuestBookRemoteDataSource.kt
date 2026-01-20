d98b74533d77e7308f7080a99a1ae8e0b09212ec
seojw0124 <wjddn2000124@gmail.com>
2026-01-21 04:44:42 +0900

    feat: DataSource에 방명록 수정 로직 추가

--- android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/guestbook/GuestBookRemoteDataSource.kt
+++ android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/guestbook/GuestBookRemoteDataSource.kt
@@ -4,6 +4,7 @@ import com.andlife.domain.error.DataError
 import com.andlife.domain.util.Result
 import com.andlife.network.api.guestbook.GuestBookRequest
 import com.andlife.network.api.guestbook.GuestBookResponse
+import com.andlife.network.api.guestbook.UpdateGuestBookRequest
 import com.andlife.network.model.PagingResponse
 import com.andlife.network.model.invitation.guestbook.CollectionResponse
 
@@ -17,4 +18,9 @@ interface GuestBookRemoteDataSource {
     ): Result<PagingResponse<GuestBookResponse>, DataError>
 
     suspend fun createGuestBook(invitationId: Long, request: GuestBookRequest): Result<GuestBookResponse, DataError>
+
+    suspend fun updateGuestBook(
+        guestBookId: Long,
+        request: UpdateGuestBookRequest
+    ): Result<GuestBookResponse, DataError>
 }
