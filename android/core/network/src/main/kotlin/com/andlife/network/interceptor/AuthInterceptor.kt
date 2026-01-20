78ef9f56d5cce95328d1af2818cdb2214290048f
pdchoirian <pdchoirian@gmail.com>
2026-01-21 01:25:08 +0900

    feat: AuthInterceptor 구현

---
+++ android/core/network/src/main/kotlin/com/andlife/network/interceptor/AuthInterceptor.kt
@@ -0,0 +1,28 @@
+package com.andlife.network.interceptor
+
+import com.andlife.network.BuildConfig
+import com.andlife.network.auth.AuthTokenProvider
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val tokenProvider: AuthTokenProvider
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val builder = originalRequest.newBuilder()
+
+        val userId = tokenProvider.getUserId()
+        val invitationIds = tokenProvider.getInvitationIds()
+
+        if (userId != null) {
+            builder.addHeader(BuildConfig.HEADER_USER_ID, userId.toString())
+        } else if (invitationIds.isNotEmpty()) {
+            val idsString = invitationIds.joinToString(",")
+            builder.addHeader(BuildConfig.HEADER_GUEST_IDS, idsString)
+        }
+
+        return chain.proceed(builder.build())
+    }
+}
