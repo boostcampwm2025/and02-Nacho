aa08e2f3ee9de5f6c550134d7af57719e6e8754a
pdchoirian <pdchoirian@gmail.com>
2026-01-22 02:24:37 +0900

    feat: InvitationParticipantRepository 페이징 도입

--- backend/src/main/kotlin/com/andlife/InvitationServer/repository/invitation/participant/InvitationParticipantRepository.kt
+++ backend/src/main/kotlin/com/andlife/InvitationServer/repository/invitation/participant/InvitationParticipantRepository.kt
@@ -1,12 +1,20 @@
 package com.andlife.InvitationServer.repository.invitation.participant
 
 import com.andlife.InvitationServer.entity.InvitationParticipant
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
 interface InvitationParticipantRepository : JpaRepository<InvitationParticipant, Long> {
 
-    @Query("SELECT p FROM InvitationParticipant p JOIN FETCH p.invitation WHERE p.user.id = :userId")
-    fun findAllByUserIdWithInvitation(@Param("userId") userId: Long): List<InvitationParticipant>
+    @Query(
+        value = "SELECT p FROM InvitationParticipant p JOIN FETCH p.invitation WHERE p.user.id = :userId",
+        countQuery = "SELECT count(p) FROM InvitationParticipant p WHERE p.user.id = :userId"
+    )
+    fun findAllByUserIdWithInvitation(
+        @Param("userId") userId: Long,
+        pageable: Pageable
+    ): Page<InvitationParticipant>
 }
