f41565108c6ce333ce24f71454d0c997e7ae317c
pdchoirian <pdchoirian@gmail.com>
2026-01-22 04:27:15 +0900

    feat: DataSource 파라미터 추가 및 반환 타입 페이징으로 변경

--- android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/InvitationRemoteDataSourceImpl.kt
+++ android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/InvitationRemoteDataSourceImpl.kt
@@ -5,6 +5,8 @@ import com.andlife.domain.error.DataError
 import com.andlife.domain.util.Result
 import com.andlife.network.api.invitation.InvitationResponse
 import com.andlife.network.api.invitation.InvitationService
+import com.andlife.network.model.PagingResponse
+import com.andlife.network.model.invitation.InvitationSummaryResponse
 import javax.inject.Inject
 
 internal class InvitationRemoteDataSourceImpl @Inject constructor(
@@ -13,6 +15,10 @@ internal class InvitationRemoteDataSourceImpl @Inject constructor(
     override suspend fun getInvitation(invitationId: Long): Result<InvitationResponse, DataError> =
         apiCall { invitationService.getInvitation(invitationId) }
 
-    override suspend fun getParticipantInvitations(): Result<List<Long>, DataError> =
-        apiCall { invitationService.getParticipantInvitations() }
+    override suspend fun getParticipantInvitations(
+        status: String,
+        page: Int,
+        size: Int
+    ): Result<PagingResponse<InvitationSummaryResponse>, DataError> =
+        apiCall { invitationService.getParticipantInvitations(status, page, size) }
 }
