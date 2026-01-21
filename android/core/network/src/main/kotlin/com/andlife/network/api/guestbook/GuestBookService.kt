e3799aac6619b6a0a0dc49a2289fbafe40608407
seojw0124 <wjddn2000124@gmail.com>
2026-01-21 10:15:15 +0900

    feat: 삭제 api service 정의

--- android/core/network/src/main/kotlin/com/andlife/network/api/guestbook/GuestBookService.kt
+++ android/core/network/src/main/kotlin/com/andlife/network/api/guestbook/GuestBookService.kt
@@ -4,6 +4,7 @@ import com.andlife.network.model.BaseResponse
 import com.andlife.network.model.PagingResponse
 import com.andlife.network.model.invitation.guestbook.CollectionResponse
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
@@ -34,4 +35,9 @@ interface GuestBookService {
         @Path("guestBookId") guestBookId: Long,
         @Body request: UpdateGuestBookRequest,
     ): BaseResponse<GuestBookResponse>
+
+    @DELETE("/api/guestbooks/{guestBookId}")
+    suspend fun deleteGuestBook(
+        @Path("guestBookId") guestBookId: Long,
+    ): BaseResponse<Long>
 }
