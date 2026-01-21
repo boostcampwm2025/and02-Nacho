6b5a2b92344825b6429b57878b8377de157e5f8f
seojw0124 <wjddn2000124@gmail.com>
2026-01-21 15:45:06 +0900

    fix: rebase 충돌 해결

--- backend/src/main/kotlin/com/andlife/InvitationServer/controller/invitation/InvitationController.kt
+++ backend/src/main/kotlin/com/andlife/InvitationServer/controller/invitation/InvitationController.kt
@@ -2,7 +2,6 @@ package com.andlife.InvitationServer.controller.invitation
 
 import com.andlife.InvitationServer.request.invitation.CreateInvitationRequest
 import com.andlife.InvitationServer.request.invitation.guestbook.GuestBookRequest
-import com.andlife.InvitationServer.request.invitation.guestbook.GuestBookUpdateRequest
 import com.andlife.InvitationServer.response.BaseResponse
 import com.andlife.InvitationServer.response.CommonResponseCode
 import com.andlife.InvitationServer.response.PagingResponse
@@ -14,13 +13,7 @@ import com.andlife.InvitationServer.service.invitation.guestbook.GuestBookServic
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/invitations")
