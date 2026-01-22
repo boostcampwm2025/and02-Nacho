029aec3e890c843e57d6310cddd9908dcde36cf0
ant5555 <ymo6886@gmail.com>
2026-01-22 18:39:58 +0900

    build: app 에 domain 의존성 임시 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -102,6 +102,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlinx.immutable)
     implementation(projects.core.deeplink)
+    implementation(projects.domain)
 
     // Hilt
     implementation(libs.hilt.android)
