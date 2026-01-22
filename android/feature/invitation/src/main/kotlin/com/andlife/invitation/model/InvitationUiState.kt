6c68d70b34f6da6c473e05ec7a5794ba30c7da2f
pdchoirian <pdchoirian@gmail.com>
2026-01-22 17:09:03 +0900

    feat: InvitationUiState 구현

--- android/feature/invitation/src/main/kotlin/com/andlife/invitation/model/InvitationUiState.kt
+++ android/feature/invitation/src/main/kotlin/com/andlife/invitation/model/InvitationUiState.kt
@@ -1,10 +1,8 @@
 package com.andlife.invitation.model
 
-import com.andlife.domain.model.invitation.InvitationSummary
 import com.andlife.ui.base.BaseUiState
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.persistentListOf
 
 data class InvitationUiState(
-    val invitationSummaries: ImmutableList<InvitationSummary> = persistentListOf(),
+    val isRefreshing: Boolean = false,
+    val selectedTab: Int = 0
 ) : BaseUiState
