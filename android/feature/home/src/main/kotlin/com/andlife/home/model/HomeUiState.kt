4d1c271d8b37c460cd8cc7730e789b00188b9ff5
ant5555 <ymo6886@gmail.com>
2026-01-22 20:45:22 +0900

    feat: upcomingInvitations ImmutableList 변경

--- android/feature/home/src/main/kotlin/com/andlife/home/model/HomeUiState.kt
+++ android/feature/home/src/main/kotlin/com/andlife/home/model/HomeUiState.kt
@@ -2,11 +2,13 @@ package com.andlife.home.model
 
 import com.andlife.model.invitation.UpcomingInvitationUiModel
 import com.andlife.ui.base.BaseUiState
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 data class HomeUiState(
     val isRefreshing: Boolean = false,
     val isRetry: Boolean = false,
-    val upcomingInvitations: List<UpcomingInvitationUiModel> = emptyList(),
+    val upcomingInvitations: ImmutableList<UpcomingInvitationUiModel> = persistentListOf(),
     val playingAudioUrl: String? = null,
     val isAudioPlaying: Boolean = false,
 ) : BaseUiState
