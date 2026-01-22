249a4712d9bc02f7171c3385e1cf0baee9be72af
pdchoirian <pdchoirian@gmail.com>
2026-01-22 10:58:02 +0900

    chore: 1번 유저로 임시 하드코딩

--- backend/src/main/kotlin/com/andlife/InvitationServer/service/invitation/InvitationService.kt
+++ backend/src/main/kotlin/com/andlife/InvitationServer/service/invitation/InvitationService.kt
@@ -9,7 +9,6 @@ import com.andlife.InvitationServer.repository.invitation.InvitationCardReposito
 import com.andlife.InvitationServer.repository.invitation.InvitationRepository
 import com.andlife.InvitationServer.repository.invitation.participant.InvitationParticipantRepository
 import com.andlife.InvitationServer.request.invitation.CreateInvitationRequest
-import com.andlife.InvitationServer.repository.invitation.participant.InvitationParticipantRepository
 import com.andlife.InvitationServer.response.invitation.AnnouncementResponse
 import com.andlife.InvitationServer.response.invitation.InvitationCardResponse
 import com.andlife.InvitationServer.response.invitation.InvitationResponse
