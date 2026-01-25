2b6abe14111dde0a05c222d53133949a4f1bf051
seojw0124 <wjddn2000124@gmail.com>
2026-01-25 17:11:54 +0900

    chore: 주석 추가

--- android/core/data/src/main/kotlin/com/andlife/data/util/media/MediaUtilModule.kt
+++ android/core/data/src/main/kotlin/com/andlife/data/util/media/MediaUtilModule.kt
@@ -8,7 +8,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class MediaUtilModule {
+abstract class MediaUtilModule { // TODO: util에 있는 모든 것들 여기에 통합해도 될 듯요.
 
     @Binds
     @Singleton
