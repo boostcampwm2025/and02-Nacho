d22e9acffeda8567a3afb4ce36bc770ce5cd2b6d
pdchoirian <pdchoirian@gmail.com>
2026-01-27 00:55:22 +0900

    feat: InvitationService 초대 참여 API 추가

--- android/core/network/src/main/kotlin/com/andlife/network/api/invitation/InvitationService.kt
+++ android/core/network/src/main/kotlin/com/andlife/network/api/invitation/InvitationService.kt
@@ -7,6 +7,7 @@ import com.andlife.network.model.invitation.InvitationCardRequest
 import com.andlife.network.model.invitation.InvitationResponse
 import retrofit2.http.Body
 import com.andlife.network.model.invitation.InvitationSummaryResponse
+import com.andlife.network.model.invitation.JoinResponse
 import com.andlife.network.model.invitation.UpcomingInvitationResponse
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -15,6 +16,11 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface InvitationService {
+
+    @POST("/api/invitations/{invitationId}/join")
+    suspend fun joinInvitation(
+        @Path("invitationId") invitationId: Long
+    ): BaseResponse<JoinResponse>
 
     @GET("/api/invitations/{invitationId}")
     suspend fun getInvitation(
