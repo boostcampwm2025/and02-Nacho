67811f4fe07e6556cfb5f96be3b443e1c2fe1591
ant5555 <ymo6886@gmail.com>
2026-01-27 18:51:19 +0900

    feat: toInvitationResponse (Backend)

--- backend/src/main/kotlin/com/andlife/nachoserver/response/invitation/InvitationResponse.kt
+++ backend/src/main/kotlin/com/andlife/nachoserver/response/invitation/InvitationResponse.kt
@@ -1,5 +1,9 @@
 package com.andlife.nachoserver.response.invitation
 
+import com.andlife.nachoserver.entity.AnnouncementSection
+import com.andlife.nachoserver.entity.Invitation
+import com.andlife.nachoserver.entity.InvitationCard
+
 data class InvitationResponse(
     val id: Long,
     val hostId: Long,
@@ -34,3 +38,43 @@ data class AnnouncementResponse(
     val content: String,
     val displayOrder: Int,
 )
+
+fun Invitation.toInvitationResponse(
+    card: InvitationCard? = null,
+    announcements: List<AnnouncementSection> = emptyList()
+): InvitationResponse {
+    return InvitationResponse(
+        id = this.id,
+        hostId = this.host.id,
+        title = this.title,
+        displayHostName = this.displayHostName,
+        hostProfileUrl = this.host.profileImageUrl,
+        thumbnailUrls = this.thumbnailUrls,
+        invitationDate = this.invitationDate.toString(),
+        startTime = this.startTime.toString(),
+        endTime = this.endTime?.toString(),
+        placename = this.placeName,
+        address = this.address,
+        lat = this.lat,
+        lng = this.lng,
+        locationGuide = this.locationGuide,
+        invitationCard = card?.let {
+            InvitationCardResponse(
+                id = it.id,
+                invitationId = this.id,
+                contentJson = it.contentJson,
+                backgroundColor = it.backgroundColor,
+                backgroundImageUrl = it.backgroundImageUrl
+            )
+        },
+        announcements = announcements.map {
+            AnnouncementResponse(
+                id = it.id,
+                invitationId = this.id,
+                title = it.title,
+                content = it.content,
+                displayOrder = it.displayOrder
+            )
+        }
+    )
+}
