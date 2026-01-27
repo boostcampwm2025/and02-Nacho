17c7abd70b2478f66121c6fbe507648af0377be1
kim dong kyung <kdkd422@naver.com>
2026-01-27 21:02:41 +0900

    build: serialization

--- android/core/datastore/build.gradle.kts
+++ android/core/datastore/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("invitation.android.library")
     id("invitation.android.hilt")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -12,6 +13,7 @@ dependencies {
     implementation(libs.datastore.preferences)
     implementation(libs.datastore.core)
 
+    implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
