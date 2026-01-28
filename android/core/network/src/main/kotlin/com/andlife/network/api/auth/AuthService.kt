b44ff2dee36dad7f9c5447f2b500e2d94d2f68aa
kim dong kyung <kdkd422@naver.com>
2026-01-29 03:59:29 +0900

    feat: AuthService

--- android/core/network/src/main/kotlin/com/andlife/network/api/auth/AuthService.kt
+++ android/core/network/src/main/kotlin/com/andlife/network/api/auth/AuthService.kt
@@ -3,6 +3,7 @@ package com.andlife.network.api.auth
 import com.andlife.network.model.BaseResponse
 import com.andlife.network.model.auth.AuthRequest
 import com.andlife.network.model.auth.AuthResponse
+import com.andlife.network.model.auth.RefreshTokenRequest
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -11,4 +12,9 @@ interface AuthService {
     suspend fun login(
         @Body request: AuthRequest
     ): BaseResponse<AuthResponse>
+
+    @POST("api/auth/reissue")
+    suspend fun reissue(
+        @Body request: RefreshTokenRequest
+    ) : BaseResponse<AuthResponse>
 }
