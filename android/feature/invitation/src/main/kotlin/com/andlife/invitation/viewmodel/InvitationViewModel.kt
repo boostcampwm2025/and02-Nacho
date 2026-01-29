11078a105b6e57e98f1f7e006f42b252198654d2
pdchoirian <pdchoirian@gmail.com>
2026-01-29 19:29:07 +0900

    feat: 딥링크로 진입했을 경우 새로고침 필요 여부를 전달하도록 구현

--- android/feature/invitation/src/main/kotlin/com/andlife/invitation/viewmodel/InvitationViewModel.kt
+++ android/feature/invitation/src/main/kotlin/com/andlife/invitation/viewmodel/InvitationViewModel.kt
@@ -130,4 +130,8 @@ class InvitationViewModel @Inject constructor(
         if (hasError) sendEffect(InvitationSideEffect.RefreshFailure)
     }
 
+    fun handleDeepLinkRefresh() {
+        sendEffect(InvitationSideEffect.RefreshFromDeepLink)
+    }
+
 }
