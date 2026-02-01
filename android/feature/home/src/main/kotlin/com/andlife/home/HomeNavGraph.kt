279d0c2d2752f1c19809619f2a6456f36939c3b0
pdchoirian <pdchoirian@gmail.com>
2026-02-01 23:29:11 +0900

    feat: HomeScreen NavigationEventhub 적용

--- android/feature/home/src/main/kotlin/com/andlife/home/HomeNavGraph.kt
+++ android/feature/home/src/main/kotlin/com/andlife/home/HomeNavGraph.kt
@@ -3,13 +3,20 @@ package com.andlife.home
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.andlife.home.screen.HomeRoute
 import com.andlife.home.screen.SettingRoute
+import com.andlife.home.viewmodel.HomeViewModel
+import com.andlife.model.util.NavigationEventhub
+import com.andlife.model.util.NavigationEventhub.RefreshTarget
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -36,6 +43,16 @@ fun NavGraphBuilder.homeNavGraph(
     onNavigateToSetting: () -> Unit,
 ) {
     composable<Home> {
+        val viewModel: HomeViewModel = hiltViewModel()
+
+        val refreshTarget by NavigationEventhub.refreshEvent.collectAsStateWithLifecycle(initialValue = null)
+
+        LaunchedEffect(refreshTarget) {
+            if (refreshTarget == RefreshTarget.HOME || refreshTarget == RefreshTarget.ALL) {
+                viewModel.handleRefresh()
+            }
+        }
+
         HomeRoute(
             snackbarHostState = snackbarHostState,
             onNavigateToCreate = onNavigateToCreate,
