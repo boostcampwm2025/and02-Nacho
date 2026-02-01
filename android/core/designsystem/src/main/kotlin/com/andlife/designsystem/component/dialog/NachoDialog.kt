d40c07a79ec7084f8eb5e87de7133c4836f7e268
kim dong kyung <kdkd422@naver.com>
2026-02-02 05:42:32 +0900

    feat: NachoDialog 모양

--- android/core/designsystem/src/main/kotlin/com/andlife/designsystem/component/dialog/NachoDialog.kt
+++ android/core/designsystem/src/main/kotlin/com/andlife/designsystem/component/dialog/NachoDialog.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.andlife.designsystem.preview.PreviewTheme
@@ -23,6 +24,7 @@ import com.andlife.designsystem.theme.NachoTheme
 @Composable
 fun NachoDialog(
     onDismiss: () -> Unit,
+    shape: Shape = NachoTheme.shapes.medium,
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit,
 ) {
@@ -35,7 +37,7 @@ fun NachoDialog(
                 modifier
                     .fillMaxWidth()
                     .padding(horizontal = NachoSpacing.large),
-            shape = NachoTheme.shapes.medium,
+            shape = shape,
             colors =
                 CardDefaults.cardColors(
                     containerColor = NachoTheme.colorScheme.backgroundTertiary,
