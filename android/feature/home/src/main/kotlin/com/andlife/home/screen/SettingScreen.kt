6963a8490a988b7110855a0240a79def361176f1
seojw0124 <wjddn2000124@gmail.com>
2026-02-04 19:19:48 +0900

    feat: 앱 버전 UI

--- android/feature/home/src/main/kotlin/com/andlife/home/screen/SettingScreen.kt
+++ android/feature/home/src/main/kotlin/com/andlife/home/screen/SettingScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.selection.TextSelectionColors
@@ -41,6 +40,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -62,6 +62,7 @@ import com.andlife.home.model.setting.SettingUiEvent
 import com.andlife.home.model.setting.SettingUiState
 import com.andlife.home.viewmodel.SettingViewModel
 import com.andlife.ui.util.collectWithLifecycle
+import com.andlife.ui.util.getAppVersion
 import com.andlife.designsystem.R as designR
 
 @Composable
@@ -169,6 +170,7 @@ fun SettingScreen(
     onEvent: (SettingUiEvent) -> Unit = {},
     modifier: Modifier = Modifier,
 ) {
+    val context = LocalContext.current
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
@@ -231,7 +233,7 @@ fun SettingScreen(
 
                 SettingSection(headerTitle = stringResource(R.string.txt_header_app_info)) { modifier ->
                     AppInfoContent(
-                        appVersion = "1.0.0",
+                        appVersion = context.getAppVersion(),
                         onClickInfo = {},
                         modifier = modifier,
                     )
