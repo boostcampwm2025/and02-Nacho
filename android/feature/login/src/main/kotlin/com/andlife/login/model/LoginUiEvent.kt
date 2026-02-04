808f874a7796f2b23ae834e27b499f31ce16a242
kim dong kyung <kdkd422@naver.com>
2026-02-04 16:41:58 +0900

    feat: 테스트 이벤트

--- android/feature/login/src/main/kotlin/com/andlife/login/model/LoginUiEvent.kt
+++ android/feature/login/src/main/kotlin/com/andlife/login/model/LoginUiEvent.kt
@@ -5,4 +5,5 @@ import com.andlife.ui.base.BaseUiEvent
 sealed interface LoginUiEvent : BaseUiEvent {
     data class SocialLoginSuccess(val accessToken: String) : LoginUiEvent
     data object GuestLogin : LoginUiEvent
+    data object TestLogin : LoginUiEvent
 }
