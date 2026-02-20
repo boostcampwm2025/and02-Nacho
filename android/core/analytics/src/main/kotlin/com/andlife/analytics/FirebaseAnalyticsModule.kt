309e2c5577eeb3c58ecf1fec2f45bf2a96819341
kim dong kyung <kdkd422@naver.com>
2026-02-21 02:27:04 +0900

    feature: 파이어베이스 애널리틱스 구현체 및 의존성 주입

---
+++ android/core/analytics/src/main/kotlin/com/andlife/analytics/FirebaseAnalyticsModule.kt
@@ -0,0 +1,34 @@
+package com.andlife.analytics
+
+import com.andlife.domain.util.AnalyticsLogger
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.analytics
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseAnalyticsProvideModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAnalytics(): FirebaseAnalytics {
+        return Firebase.analytics
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class FirebaseAnalyticsBindModule {
+    @Binds
+    @Singleton
+    abstract fun bindFirebaseAnalyticsLogger(
+        firebaseAnalyticsLogger: FirebaseAnalyticsLogger
+    ): AnalyticsLogger
+}
+
