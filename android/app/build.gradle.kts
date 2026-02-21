4056fbf4554d1ea80b4a65bea425d5c9241971bd
ant5555 <ymo6886@gmail.com>
2026-02-21 16:23:44 +0900

    build: 버전네임 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.andlife.nacho"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
