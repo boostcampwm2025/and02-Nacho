600bc6165bdeefec17b7b9fe793b652c9d1c674a
jm3789 <jm37890@gmail.com>
2026-02-28 15:23:05 +0900

    build: 의존성 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -138,6 +138,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.crashlytics)
     implementation(libs.firebase.analytics)
+    implementation(libs.firebase.messaging)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
