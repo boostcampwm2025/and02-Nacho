7df128e6edfa0270a422f26c754051d6d90bd9d2
ant5555 <ymo6886@gmail.com>
2026-03-01 13:07:00 +0900

    feat: 기본으로 room 표시

--- android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/InvitationRemoteMediator.kt
+++ android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/InvitationRemoteMediator.kt
@@ -4,7 +4,6 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
-import androidx.paging.RemoteMediator.InitializeAction
 import androidx.room.withTransaction
 import com.andlife.data.repository.invitation.mapper.toEntity
 import com.andlife.database.InvitationDatabase
@@ -25,7 +24,13 @@ class InvitationRemoteMediator(
 
     private val dao = database.invitationSummaryDao()
 
-    override suspend fun initialize(): InitializeAction = InitializeAction.LAUNCH_INITIAL_REFRESH
+    override suspend fun initialize(): InitializeAction {
+        return if (dao.count(status.name, isMyInvitation) > 0) {
+            InitializeAction.SKIP_INITIAL_REFRESH
+        } else {
+            InitializeAction.LAUNCH_INITIAL_REFRESH
+        }
+    }
 
     override suspend fun load(
         loadType: LoadType,
--- android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/UpcomingInvitationRemoteMediator.kt
+++ android/core/data/src/main/kotlin/com/andlife/data/datasource/remote/invitation/UpcomingInvitationRemoteMediator.kt
@@ -20,7 +20,12 @@ class UpcomingInvitationRemoteMediator(
 
     private val dao = database.upcomingInvitationDao()
 
-    override suspend fun initialize(): InitializeAction = InitializeAction.LAUNCH_INITIAL_REFRESH
+    override suspend fun initialize(): InitializeAction {
+        return if (dao.count() > 0)
+            InitializeAction.SKIP_INITIAL_REFRESH
+        else
+            InitializeAction.LAUNCH_INITIAL_REFRESH
+    }
 
     override suspend fun load(
         loadType: LoadType,
