b0d8a86730400d2a8143c3fb7831e0ee21be9803
ant5555 <ymo6886@gmail.com>
2026-03-01 14:37:46 +0900

    feat: database에 GuestBook 추가

--- android/core/database/src/main/kotlin/com/andlife/database/DatabaseConstants.kt
+++ android/core/database/src/main/kotlin/com/andlife/database/DatabaseConstants.kt
@@ -2,8 +2,9 @@ package com.andlife.database
 
 object DatabaseConstants {
     const val DATABASE_NAME = "nacho.db"
-    const val DATABASE_VERSION = 1
+    const val DATABASE_VERSION = 2
 
     const val TABLE_INVITATION_SUMMARY = "invitation_summary"
     const val TABLE_UPCOMING_INVITATION = "upcoming_invitation"
+    const val TABLE_GUEST_BOOK = "guest_book"
 }
--- android/core/database/src/main/kotlin/com/andlife/database/InvitationDatabase.kt
+++ android/core/database/src/main/kotlin/com/andlife/database/InvitationDatabase.kt
@@ -4,8 +4,10 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.andlife.database.converter.Converters
+import com.andlife.database.dao.GuestBookDao
 import com.andlife.database.dao.InvitationSummaryDao
 import com.andlife.database.dao.UpcomingInvitationDao
+import com.andlife.database.entity.GuestBookEntity
 import com.andlife.database.entity.InvitationSummaryEntity
 import com.andlife.database.entity.UpcomingInvitationEntity
 
@@ -13,6 +15,7 @@ import com.andlife.database.entity.UpcomingInvitationEntity
     entities = [
         InvitationSummaryEntity::class,
         UpcomingInvitationEntity::class,
+        GuestBookEntity::class,
     ],
     version = DatabaseConstants.DATABASE_VERSION,
     exportSchema = false
@@ -21,4 +24,5 @@ import com.andlife.database.entity.UpcomingInvitationEntity
 abstract class InvitationDatabase : RoomDatabase() {
     abstract fun invitationSummaryDao(): InvitationSummaryDao
     abstract fun upcomingInvitationDao(): UpcomingInvitationDao
+    abstract fun guestBookDao(): GuestBookDao
 }
