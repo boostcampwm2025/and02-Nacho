b8abc22223c7540a2ba8c6465018c31b0cec403c
kim dong kyung <kdkd422@naver.com>
2026-03-16 08:49:54 +0900

    move

--- android/app/src/main/kotlin/com/andlife/nacho/util/NavigationSuiteTypeUtil.kt
+++
@@ -1,9 +0,0 @@
-package com.andlife.nacho.util
-
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
-
-internal val NavigationSuiteType.isNavigationBar
-    get() =
-        this == NavigationSuiteType.ShortNavigationBarCompact ||
-            this == NavigationSuiteType.ShortNavigationBarMedium ||
-            this == NavigationSuiteType.NavigationBar
