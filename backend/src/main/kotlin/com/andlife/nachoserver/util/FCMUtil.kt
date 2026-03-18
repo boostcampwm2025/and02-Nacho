397e40d1eb117d0a811d6f2a4d5e78155a2bd5b4
jm3789 <jm37890@gmail.com>
2026-03-18 10:16:26 +0900

    feat: fcm util 추가

---
+++ backend/src/main/kotlin/com/andlife/nachoserver/util/FCMUtil.kt
@@ -0,0 +1,32 @@
+package com.andlife.nachoserver.util
+
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import com.google.firebase.messaging.Notification
+import org.springframework.stereotype.Component
+
+@Component
+class FCMUtil {
+    // 특정 FCM 토큰으로 메시지 전송
+    fun sendToToken(fcmToken: String, title: String, body: String) {
+        val message = Message.builder()
+            .setToken(fcmToken)
+            .setNotification(
+                Notification.builder()
+                    .setTitle(title)
+                    .setBody(body)
+                    .build()
+            )
+            .build()
+
+        val response = FirebaseMessaging.getInstance().send(message)
+        println("Successfully sent message: $response")
+    }
+
+    // 하나의 메시지를 여러 기기에 전송
+    fun sendToTokens(fcmTokens: List<String>, title: String, body: String) {
+        fcmTokens.forEach { token ->
+            sendToToken(token, title, body)
+        }
+    }
+}
